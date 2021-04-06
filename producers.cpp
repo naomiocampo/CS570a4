3746be9f053ba04cab63cf06e0c37259a9414db9
naomiocampo <72893899+naomiocampo@users.noreply.github.com>
2021-04-06 02:42:29 -0700

    Create producers.cpp

---
+++ producers.cpp
@@ -0,0 +1,41 @@
+// x <= 10 candies on the belt at a time
+// crunchy frog bites  a <= 3 on a belt at a time
+// produced FIFO order
+// everytime belt has an addition or removal, there should be an indication
+//stop production once 100 candies are produced
+// exit after 100 candies
+
+pseudo code
+define capacity of queue ( 10 elements )
+  
+  
+   if ( frog bites <= 3 )
+    then continue
+   if ( frog bites = 3 )
+      dont allow more frog bites
+   if ( frog bites > 3 )
+        Stop pls 
+   if ( candy production > 100 )
+     continue
+    if ( candy production !> 100 )
+      STOP
+     
+   
+queue
+initialize variables:
+  integer *array ( stores queue elements )
+  integer capacity ( max capacity of queue )
+  int front ( front elements, not sure if this is needed )
+      ^^ will probably be used to notify when product is added or removed
+  int rear ( last element in the queue ) 
+    ^^ same w this
+   int count ( current size of queue )
+    ^^ unsure if needed
+    
+   
+  queue constructor to initialize queue
+ queue::queue(int size){}
+ 
+  queue destructor
+  
+  
